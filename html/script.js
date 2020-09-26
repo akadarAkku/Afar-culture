f569a30003865ec25727c641a04d86c45b6cd983
Ibrahim Akader <akadar.akku121@gmail.com>
2020-09-26 11:02:56 +0200

    updated navbarss

--- html/script.js
+++ html/script.js
@@ -86,3 +86,15 @@ var documentosRef = firebase.database().ref('documentos');
           $(this).addClass('active');
         });
       });
+
+      
+//navbar
+$(document).ready(function() {   
+    var sideslider = $('[data-toggle=collapse-side]');
+    var sel = sideslider.attr('data-target');
+    var sel2 = sideslider.attr('data-target-2');
+    sideslider.click(function(event){
+        $(sel).toggleClass('in');
+        $(sel2).toggleClass('out');
+    });
+});
